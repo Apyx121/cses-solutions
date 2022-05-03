698ecf781b945ac7fe3596bd016cc9ff86eb2808
Apyx121 <65997053+Apyx121@users.noreply.github.com>
2022-05-03 17:02:38 +0300

    Create Exponentiation II.cpp

---
+++ Mathematics/Exponentiation II.cpp
@@ -0,0 +1,31 @@
+//https://cses.fi/problemset/task/1712
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int modulo = 1e9 + 7;
+ 
+long long binpow(long long a, long long b, long long mod) {
+    long long res = 1;
+    while(b > 0) {
+        if(b & 1) {
+            res *= a;
+            res %= mod;
+        }
+        a *= a;
+        a %= mod;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        cout << binpow(x, binpow(y, z, modulo-1), modulo) << "\n";
+    }
+    return 0;
+}
